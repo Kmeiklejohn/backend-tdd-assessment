c75b6966778cacfa83d7b9f56e10fe0aa01e6b1f
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2018-12-05 15:20:24 -0500

    tdd

--- echo.py
+++ echo.py
@@ -6,17 +6,36 @@ __author__ = "???"
 
 
 import sys
+import argparse
 
 
 def create_parser():
-    """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser.add_argument("-u", "--upper", action="store_true", help="convert text to uppercase")
+    parser.add_argument("-l", "--lower", action="store_true", help="convert text to lower")
+    parser.add_argument("-t", "--title", action="store_true", help="convertvtext to titlecase")
+    parser.add_argument("text", type=str, help="Text to be manipulated")
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    parser = create_parser()
+    answer = parser.parse_args(args)
+    args_length = len([x for x in (answer.lower, answer.upper, answer.title)if x is True])
+    output = ''
+
+    if answer.upper:
+        output = answer.text.upper()
+    if answer.lower:
+        output = answer.text.lower()
+    if answer.title:
+        output = answer.text.title()
+    if args_length == 0:
+        output = answer.text
+    
+    return output
 
 
 if __name__ == '__main__':
-    pass
+    print(main(sys.argv[1:]))
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,11 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import unittest
 import echo
+import unittest
+import subprocess
 
 # Your test case class goes here
+class TestEcho(unittest.TestCase):
+    
+    
+    def setUp(self):
+        self.parser = echo.create_parser()
+    
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+    # Run the command `python ./echo.py -h` in a separate process, then
+    # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
+    def test_upper(self):
+        args_list = ["--u", "hello" ]
+        args = self.parser.parse_args(args_list)
+        self.assertTrue(args.upper)
 
 
+        self.assertEqual(echo.main(args_list), "HELLO")
+
+    def test_upper_long(self):
+        args_list = ["--upper", "hello" ]
+        args = self.parser.parse_args(args_list)
+        self.assertTrue(args.upper)
+
+
+        self.assertEqual(echo.main(args_list), "HELLO") 
+
+    def test_lower(self):
+        args_list = ["--l", "HELLO" ]
+        args = self.parser.parse_args(args_list)
+        self.assertTrue(args.lower)
+
+
+        self.assertEqual(echo.main(args_list), "hello")   
+
+    def test_lower_long(self):
+        args_list = ["--lower", "HELLO"]
+        args = self.parser.parse_args(args_list)
+        self.assertTrue(args.lower)
+        self.assertEqual(echo.main(args_list), "hello")
+    
+    def test_title(self):
+        args_list = ["--t", "heLLo"]
+        args = self.parser.parse_args(args_list)
+        self.assertTrue(args.title)
+        self.assertEqual(echo.main(args_list), "Hello")
+
+    def test_title_long(self):
+        args_list = ["--title", "heLLo"]
+        args = self.parser.parse_args(args_list)
+        self.assertTrue(args.title)
+        self.assertEqual(echo.main(args_list), "Hello")
+
+    
 if __name__ == '__main__':
     unittest.main()
